7f495f8b945717ee8303cbdda201986e973ead10
Vanquang <vanquangna@gmail.com>
2017-09-10 15:06:30 -0700

    footer.php

--- wp-content/themes/storefront/footer.php
+++ wp-content/themes/storefront/footer.php
@@ -6,34 +6,59 @@
  *
  * @package storefront
  */
-
 ?>
 
-		</div><!-- .col-full -->
-	</div><!-- #content -->
+</div><!-- .col-full -->
+</div><!-- #content -->
 
-	<?php do_action( 'storefront_before_footer' ); ?>
+<?php do_action('storefront_before_footer'); ?>
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="col-full">
+<footer id="colophon" class="site-footer" role="contentinfo">
+    <div class="col-full">
 
-			<?php
-			/**
-			 * Functions hooked in to storefront_footer action
-			 *
-			 * @hooked storefront_footer_widgets - 10
-			 * @hooked storefront_credit         - 20
-			 */
-			do_action( 'storefront_footer' ); ?>
+        <?php
+        /**
+         * Functions hooked in to storefront_footer action
+         *
+         * @hooked storefront_footer_widgets - 10
+         * @hooked storefront_credit         - 20
+         */
+        ///do_action( 'storefront_footer' );
+        echo '<h3>Học lớp lập trình onlines miễn phí.</h3><br/>Hướng dẫn xây dựng website bán hàng và thanh toán trực tuyến.<br/>Website :<br/>Fanpage :<br/>';
+        echo '<a href="https://vi-vn.facebook.com/"><img src="images\facebook.png"></a> <a href="https://twitter.com/"> <img  src="images\quadat.png"></a> <a href="https://www.google.com.vn/?gfe_rd=cr&ei=XUYRWcizLoLj8we4haSICw/"><img src="images\goog.png"></a>';
+        ?>
 
-		</div><!-- .col-full -->
-	</footer><!-- #colophon -->
+    </div><!-- .col-full -->
+</footer><!-- #colophon -->
 
-	<?php do_action( 'storefront_after_footer' ); ?>
+<?php do_action('storefront_after_footer'); ?>
 
 </div><!-- #page -->
 
-<?php wp_footer(); ?>
+<?php //wp_footer();  ?>
+<!-- Scroll back to top -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+<div id="backtotop"> 
+    <a href="javascript:void(0)" class="backtotop"></a> 
+</div>                 
+<script type="text/javascript">
+    $(function () {
+        $("#backtotop").hide();
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 500) {
+                $('#backtotop').fadeIn();
+            } else {
+                $('#backtotop').fadeOut();
+            }
+        });
+    });
+    jQuery('.backtotop').click(function () {
+        jQuery('html, body').animate({
+            scrollTop: 0
+        }, 'slow');
+    });
+</script>
+<!-- End of Scroll back to top --> 
 
 </body>
 </html>
