ff507c8b11682bdb64ee34aa660974264dcb41e6
Pham Hieu <mrhieuit91@mail.com>
2017-09-22 15:31:46 +0700

    thêm widget tin mới

--- wp-content/themes/storefront-child/functions.php
+++ wp-content/themes/storefront-child/functions.php
@@ -664,4 +664,125 @@ class FGC_Comment_Widget extends WP_Widget {
 
 }
 
+/* ------------------------------------------------------------------------------------------------------- */
+
+/**
+ *  RECENT POST WIDGET
+ */
+add_action('widgets_init', 'fgc_create_recent_news_widget');
+
+function fgc_create_recent_news_widget() {
+    register_widget('FGC_Recent_News_Posts');
+}
+
+class FGC_Recent_News_Posts extends WP_Widget {
+
+    /**
+     * Sets up a new Recent Posts widget instance.
+     *
+     * @since 2.8.0
+     * @access public
+     */
+    public function __construct() {
+        $widget_ops = array(
+            'fgc_recent_news_post', 'Các đánh giá mới nhất',
+            'classname' => 'widget_recent_news_entries',
+            'description' => __('Các tin tức mới '),
+            'customize_selective_refresh' => true,
+        );
+        parent::__construct('recent-news_posts', __('Tin Tức mới'), $widget_ops);
+        $this->alt_option_name = 'widget_recent_news_entries';
+    }
+
+    
+    public function widget($args, $instance) {
+        if (!isset($args['widget_id'])) {
+            $args['widget_id'] = $this->id;
+        }
+
+        $title = (!empty($instance['title']) ) ? $instance['title'] : __('Tin Tức mới');
+
+        /** This filter is documented in wp-includes/widgets/class-wp-widget-pages.php */
+        $title = apply_filters('widget_title', $title, $instance, $this->id_base);
+
+        $number = (!empty($instance['number']) ) ? absint($instance['number']) : 5;
+        if (!$number)
+            $number = 5;
+        $show_date = isset($instance['show_date']) ? $instance['show_date'] : false;
+
+       
+        $r = new WP_Query(apply_filters('widget_posts_args', array(
+                    'posts_per_page' => $number,
+                    'no_found_rows' => true,
+                    'post_status' => 'publish',
+                    'ignore_sticky_posts' => true
+        )));
+
+        if ($r->have_posts()) :
+            ?>
+            <?php echo $args['before_widget']; ?>
+            <?php
+            if ($title) {
+                echo $args['before_title'] . $title . $args['after_title'];
+            }
+            ?>
+            <style>
+                .contact-boder-menu{font-weight: bold;}
+                .contact-boder-menu ul {
+                    margin-left: 15px;
+                    
+                }
+                .contact-boder-menu ul li a{
+                    text-decoration: none !important; 
+                }
+                .contact-boder-menu ul li a:hover {
+                    text-decoration: underline !important;        
+                }
+            </style>
+            <div class="contact-boder-menu" style="border: dashed 1px">
+                <ul style="list-style-type: circle; color: #000 !important;">
+                    <?php while ($r->have_posts()) : $r->the_post(); ?>
+                        <li>
+                            <a href="<?php the_permalink(); ?>" style="color: #000 !important"><?php get_the_title() ? the_title() : the_ID(); ?></a>
+                            <?php if ($show_date) : ?>
+                                <span class="post-date"><?php echo get_the_date(); ?></span>
+                            <?php endif; ?>
+                        </li>
+                    <?php endwhile; ?>
+                </ul>
+            </div>
+            <?php echo $args['after_widget']; ?>
+            <?php
+            // Reset the global $the_post as this query will have stomped on it
+            wp_reset_postdata();
+
+        endif;
+    }
+    
+    public function update($new_instance, $old_instance) {
+        $instance = $old_instance;
+        $instance['title'] = sanitize_text_field($new_instance['title']);
+        $instance['number'] = (int) $new_instance['number'];
+        $instance['show_date'] = isset($new_instance['show_date']) ? (bool) $new_instance['show_date'] : false;
+        return $instance;
+    }
+    
+    public function form($instance) {
+        $title = isset($instance['title']) ? esc_attr($instance['title']) : '';
+        $number = isset($instance['number']) ? absint($instance['number']) : 5;
+        $show_date = isset($instance['show_date']) ? (bool) $instance['show_date'] : false;
+        ?>
+        <p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" /></p>
+
+        <p><label for="<?php echo $this->get_field_id('number'); ?>"><?php _e('Number of posts to show:'); ?></label>
+            <input class="tiny-text" id="<?php echo $this->get_field_id('number'); ?>" name="<?php echo $this->get_field_name('number'); ?>" type="number" step="1" min="1" value="<?php echo $number; ?>" size="3" /></p>
+
+        <p><input class="checkbox" type="checkbox"<?php checked($show_date); ?> id="<?php echo $this->get_field_id('show_date'); ?>" name="<?php echo $this->get_field_name('show_date'); ?>" />
+            <label for="<?php echo $this->get_field_id('show_date'); ?>"><?php _e('Display post date?'); ?></label></p>
+        <?php
+    }
+
+}
+
 //remove_action( 'woocommerce_before_main_content','woocommerce_breadcrumb', 20, 0);
