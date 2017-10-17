54dc3e5dc1040a338f2b898ece53644fb39746d4
quangtrg <quangtr.work@gmail.com>
2017-10-17 15:13:16 +0700

    di chuyeenr

---
+++ buy-product-for-cart.php
@@ -0,0 +1,18 @@
+<?php
+ 	require(dirname( __FILE__ ). '\wp-blog-header.php' );
+	//$product_id= get_the_ID();
+	$product_id= $_REQUEST['products'];
+	//echo $product_id;
+	WC()->cart->add_to_cart( $product_id, 1 );
+
+ 	//echo "Đã có trong giỏ hàng";
+// add_filter('add_to_cart_redirect', 'custom_add_cart_redirect');
+  
+//function custom_add_cart_redirect() {
+	//$product_id = $_REQUEST['products'];
+	//echo "Ten toi la {$_REQUEST['products']}  ";
+    //$woocommerce->cart->add_to_cart( $product_id ); $_POST['product_id'];
+ 	//PRINT_R($_REQUEST);
+//}
+
+?>
